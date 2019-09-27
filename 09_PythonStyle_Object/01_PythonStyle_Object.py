fe277368a2e79825bdbe5f0468a166b5d5178f84
changja88 <DJdnls88!!>
2019-09-27 10:27:13 +0900

    save

--- 09_PythonStyle_Object/01_PythonStyle_Object.py
+++ 09_PythonStyle_Object/01_PythonStyle_Object.py
@@ -72,3 +72,85 @@ print(abs(v1))
 
 print(bool(v1))
 print(bool(Vector2d(0, 0)))
+
+# 포맷된 출력
+# - format() 내장 함수와 str.format() 메서드는 실제 포맷 작업을 __format__ 메서드에 위임한다
+# - format_spec은 포맷 명시자(format specifier)로서, 다음 두 가지 방법중 하나를 통해 지정한다
+#   - format(my_obj, format_spec) 의 두번째 인수
+#   - str.format()에 사용된 포맷 문자열 안에 {}로 구분한 대체 필드 안에서 콜론 뒤의 문자열
+brl = 1 / 2.43
+print(brl)
+print(format(brl, '0.4f'))
+
+print('1 BRL = {rate:0.2f} USD'.format(rate=brl))
+# - 0.2f가 포맷 명시자다. 대체 필드 안에 있는 rate 문자열을 필드명이라고 한다. 이 문자열은 포맷 명시자와 상관 없지만
+#   format() 인수중 어느 인수가 대체 필드에 들어갈 것인지 결정한다
+# - 이 0.2f와 같은 포맷 명시자에 사용된 표기법을 '포맷 명시 간이 언어(Format Specification Mini-Language)라고 한다
+# - 몇몇 내장 자료형은 포맷 명시 간이 언어에 자신만의 고유한 표현 코드를 가지고 있다
+#   - int형의 경우 이진수를 나타내는 'b', 16진수를 나타내년 'x'코드를 지원 하고
+#   - float 형의 경우 고정소수점을 나타내는 'f', 백분율을 나타내는 '%'코드를 지원한다
+print(format(42, 'b'))
+print(format(2 / 4, '.1%'))
+
+# - 각 클래스가 format_spec인수를 자신이 원하는 대로 해석해서 포맷 명시 간이 언어를 확장 할 수 있다
+#   - 예를 들어 datetime모듈의 클래스들은 자신의 __format__메서드에서 strtime()함수와 동일한 포맷 코드를 사용한다
+from datetime import datetime
+
+now = datetime.now()
+print(format(now, '%H:%M:%S'))
+print("It's now {:%I:%M %P}".format(now))
+
+# - 클래스에서 __format__ 메서드를 정의하지 않으면, object에서 상속받은 메서드가 str(my_object)를 반환한다
+# - Vector2d 는 __str__을 정의하고 있으므로 아래와 같이 실행 된다
+v1 = Vector2d(3, 4)
+print(format(v1))
+
+
+# - 그러나 이때 포맷 명시자를 사용하면 TypeError를 발생한다
+# format(v1, '.3f')
+# - Vector2d 에 아래를 추가 하면 해결되고 __str__이 불리지 않기 때문에 출력이 달라진다
+# def __format__(self, format_spec):
+#     return "abc"
+
+
+# 해시 가능한 Vector2d
+# - 지끔까지 정의한 Vector2d는 해시 할 수 없다. 그러므로 집합 안의 항목으로 사용할 수 없다
+# set = {Vector2d(1, 2), Vector2d(4, 4)} -> unHashable type Error
+# - 집합안의 항목으로 사용 하기 위해서는 __eq__, __hash__를 구현 해야 한다
+# - 해시 가능하다는 말의 의미는 -> 객체를 불변형으로 만들어야 한다
+
+# 객체를 불변형으로 만들기
+class Vector2d2:
+    typecode = 'd'
+
+    def __init__(self, x, y):
+        self.__x = float(x)  # 언더바 두개로 시작하는 속성은 비공개로 만든다
+        self.__y = float(y)
+
+    @property  # 프로퍼티의 getter메서드를 나타낸다
+    def x(self):  # 자신이 노출시키는 공개 속성명을 따라 getter메서드의 이름을 지정한다
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__x
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+    # __hash__
+    #   - int형을 반환해야 한다
+    #   - 동일하다고 판단되는 객체는 동일한 해시값을 가져야 하므로 __eq__메서드가 사용하는 개체의 속성을 이용해서
+    #     해시를 계산하는 것이 이상적이다
+    #   - 문서에서는 요소의 해시에 비트 타위 XOR연산자(^)를 사용하는 것을 권한장다
+
+
+# 이제 집합 안의 항목으로 사용 할 수 있다
+v1 = Vector2d2(1, 2)
+v2 = Vector2d2(3, 4)
+set([v1, v2])
